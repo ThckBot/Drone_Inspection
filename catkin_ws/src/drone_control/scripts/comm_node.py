65c6b0b9082b27de0169bdf83d79983681e0848f
cathy-guo <68700310+cathy-guo@users.noreply.github.com>
2023-02-28 17:09:23 -0500

    Update comm_node.py

--- catkin_ws/src/drone_control/scripts/comm_node.py
+++ catkin_ws/src/drone_control/scripts/comm_node.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import rospy
 from drone_fsm import *
 from std_srvs.srv import Empty, EmptyResponse
@@ -57,16 +58,20 @@ def comm_node():
     drone = DroneFSM()
 
     ## MILESTONE 2 ##
+    
     drone.arm()
-    drone.takeoff(1.5) # m
-    drone.hover(10.0) # s
-    drone.land()
+    #drone.takeoff(1.5) # m
+    #drone.hover(10.0) # s
+    #drone.land()
 
     rospy.spin()
 
 if __name__ == '__main__':
+    print("in main")
     try:
         comm_node()
     except rospy.ROSInterruptException:
+        print("didnt make it in comm_node")
         pass
+    
     
