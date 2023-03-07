eab5fb5f7e682835fafeeb0bbd648e237d8dbf38
rob498 <ketanvasudeva@gmail.com>
2023-03-07 12:53:24 -0500

    Added initial script file, not yet working code skeleton

---
+++ catkin_ws/src/drone_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo chmod 666 /dev/ttyTHS1
+
+# Launch roscore in its own terminal
+# gnome-terminal -- roscore
+gnome-terminal --command="bash -c 'cd ~/Drone_Inspection/catkin_ws; roscore; $SHELL'"
+
+# Launch the px4
+sleep 5
+# gnome-terminal -- roslaunch mavros px4.launch
+gnome-terminal --command=="bash -c 'cd ~/Drone_Inspection/catkin_ws; roslaunch mavros px4.launch; $SHELL'"
+
+# Publish odometry pose at 100 Hz note odometry pose is 331
+# sleep 2
+# gnome-terminal -- rosservice call /mavros/set_message_interval 331 100
+
+# Check the odometry in is working
+# sleep 2
+# rostopic hz /mavros/odometry/in
+
