2f1270582a9c3677f6d6cd77c6afef19a827cf0e
rob498 <ketanvasudeva@gmail.com>
2023-03-16 14:13:52 -0400

    modified drone_setup.sh file to be compatible with milestone 2 setup

--- catkin_ws/src/drone_setup.sh
+++ catkin_ws/src/drone_setup.sh
@@ -9,12 +9,12 @@ gnome-terminal --command="bash -c 'cd ~/Drone_Inspection/catkin_ws; roscore; $SH
 # Launch the px4
 sleep 5
 # gnome-terminal -- roslaunch mavros px4.launch
-gnome-terminal --command="bash -c 'cd ~/Drone_Inspection/catkin_ws; roslaunch mavros px4.launch; $SHELL'"
+gnome-terminal --command="bash -c 'cd ~/thirdparty/vio_ws/devel/; source setup.bash; roslaunch px4_realsense_bridge bridge_mavros.launch; $SHELL'"
 
 # Publish odometry pose at 100 Hz note odometry pose is 331
 # sleep 2
 # gnome-terminal -- rosservice call /mavros/set_message_interval 331 100
-gnome-terminal --command="bash -c 'cd ~/Drone_Inspection/catkin_ws; rosservice call /mavros/set_message_interval 331 100 $SHELL'"
+# gnome-terminal --command="bash -c 'cd ~/Drone_Inspection/catkin_ws; rosservice call /mavros/set_message_interval 331 100 $SHELL'"
 
 # Check the odometry in is working
 # sleep 2
